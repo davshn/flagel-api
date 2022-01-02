1cc4837059e35ca512143a6a4b58412c0801dde8
Hernán David Figueroa <davshn@gmail.com>
2022-01-02 11:35:32 -0500

    port changed

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require('./config/app.js');
 const { conn } = require('./config/db.js');
 
 conn.sync({ force: true }).then(() => {
-  app.listen(3001, () => {
+  app.listen(process.env.PORT || 3001, () => {
     console.log('%s listening at 3001'); 
   });
 });
