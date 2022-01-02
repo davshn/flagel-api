8b1c8214c290e1ba0cc600ab1e21fa42a7e3a23c
Hernán David Figueroa <davshn@gmail.com>
2022-01-02 11:50:36 -0500

    favicon fixed

--- src/config/app.js
+++ src/config/app.js
@@ -6,9 +6,9 @@ const { buildSchema, print } = require('graphql');
 const { graphqlHTTP } = require('express-graphql');
 const typeDefs = require('../app/graph/schemas/index');
 const resolvers = require('../app/graph/resolvers/index');
+const favicon = require("serve-favicon");
 require('./db');
 
-//const favicon = require("serve-favicon");
 const schemas = buildSchema(print(typeDefs));   //Crea los esquemas a partir de las definiciones de tipo
 /*
 const { SubscriptionManager } = require('graphql-subscriptions');
@@ -18,12 +18,12 @@ const { SubscriptionServer } = require('subscriptions-transport-ws');
 
 const app = express();    //Crea el servidor
 app.name = 'API Fagela';
+app.use(favicon(__dirname + '/favicon.png'));
 
 app.use(cors());
 app.use(morgan('common'));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json({ limit: '50mb' }));
-//app.use(favicon(__dirname + '/public/favicon.png'));
 app.use(cookieParser());
 
 app.use('/', graphqlHTTP({
